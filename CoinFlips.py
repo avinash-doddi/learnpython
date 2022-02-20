3cd6811c2a713003ce8d88f90de8fac0a6dd38bf
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-02-20 12:42:56 +0530

    Add files via upload

---
+++ CoinFlips.py
@@ -0,0 +1,23 @@
+""" Coin Flips - by Avinash Doddi """
+
+import random
+
+def randomflips():
+	rand = random.random() # generates a random value
+	if (rand > 0.4): return True
+	else: return False
+	
+def flips(n):
+	heads = 0
+	tails = 0
+	for i in range(n):
+		if (randomflips()):
+			tails += 1;
+		else:
+			heads += 1
+	print("tails:" + str(tails) + " heads:" + str(heads))	
+	
+
+if __name__ == '__main__':
+	n = int(input("Enter number of flips:"))
+	flips(n)
