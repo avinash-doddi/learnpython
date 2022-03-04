4e22a05d3cee743990e19760008df6dacdf78b90
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-03-04 18:43:20 +0530

    Update Binary_to_Decimal_and_Back.py

--- Binary_to_Decimal_and_Back.py
+++ Binary_to_Decimal_and_Back.py
@@ -1,7 +1,7 @@
 """Binary to Decimal and Back Converter by avinash-doddi"""
 
 def binary_to_Decimal(binary): # a function to print decimal
-    print(int(binary, 2))
+    print(int(binary, 2)) # this converts binary to decimal
 
 def Decimal_to_binary(decimal): # a function to print binary
     print(bin(decimal)[2:])
