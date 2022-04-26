94e16918eb6973580c73c93ae3960bafd1ea6ba4
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-04-26 23:13:33 +0530

    Create HappyNumbers.py

---
+++ HappyNumbers.py
@@ -0,0 +1,14 @@
+# for reference of problem : https://github.com/avinash-doddi/Projects-Solutions
+"""Happy Number by Avinash Doddi"""
+for _ in range(int(input())):
+    n = input("Enter a number: ")
+    sumval = 0
+    while(True):
+      while(n > 0):
+        sumval += (n % 10) ** 2;
+        n //= 10;
+        
+      n = sumval;
+      if (n < 10): break;
+  if (n == 1): print("YES")
+  else: print("NO")
